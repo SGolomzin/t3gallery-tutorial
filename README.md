7f8dc8609721b450ae355145a816858b87483981
SGolomzin <s.golomzin@gmail.com>
2024-09-28 15:24:06 +0300

    Finalize modal and parallel image routes

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 - [x] "taint" (server-only)
 - [x] use Next/Image component
 - [ ] Error management (w/ Sentry) - can't be implemented in Russia due to sanctions
-- [ ] Routing/image page (parallel route)
+- [x] Routing/image page (parallel route)
 - [ ] Delete button (w/ Server Actions)
 - [ ] Analytics (posthog)
 - [ ] Ratelimiting (upstash)
--- src/components/full-image-page.tsx
+++ src/components/full-image-page.tsx
@@ -1,7 +1,9 @@
 import { getImage } from "~/server/queries";
+import { clerkClient } from "@clerk/nextjs/server";
 
 export default async function FullPageImageView(props: { id: number }){
-		const image = await getImage(props.id);
+	const image = await getImage(props.id);
+	const uploaderInfo = await clerkClient.users.getUser(image.userId);
 
 	return (
 		<div className="flex w-full h-full min-w-0">
@@ -9,8 +11,21 @@ export default async function FullPageImageView(props: { id: number }){
 				<img src={image.url} alt={image.name} className="object-contain" />
 			</div>
 
-			<div className="w-48 flex flex-col flex-shrink-0 border-l">
-				<div className={"text-xl font-bold"}>{image.name}</div>
+			<div className="flex w-96 flex-col border-l">
+				<div className={"text-lg border-b p-2 text-center"}>{image.name}</div>
+				<div className="flex flex-col p-2">
+					<span>Uploaded By</span>
+					<span>{uploaderInfo.fullName}</span>
+				</div>
+				<div className="flex flex-col p-2">
+					<span>Created On</span>
+					<span>{new Date(image.createdAt).toLocaleDateString()}</span>
+				</div>
+				<div className="flex flex-col p-2">
+					<span>Uploaded By:</span>
+					<span>{uploaderInfo.fullName}</span>
+				</div>
+
 			</div>
 		</div>
 	);
